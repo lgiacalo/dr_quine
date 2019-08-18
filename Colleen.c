4b2df3a061e1d53c9bf41edd600e4e7ae0edc14d
Lena GIACALONE <lgiacalo@student.42.fr>
2019-08-18 19:22:54 +0200

    fix: format commentaire

--- Colleen.c
+++ Colleen.c
@@ -1,13 +1,17 @@
-//Commentaire1
+/*
+	Commentaire1
+*/
 
 void	function1()
 {
-	char *c="//Commentaire1%1$c%1$cvoid%2$cfunction1()%1$c{%1$c%2$cchar *c=%3$c%s%3$c;%1$c%2$cprintf(c,10,9,34,c);%1$c}%1$c%1$cmain()%1$c{%1$c%2$c//Commentaire2%1$c%2$cfunction1();%1$c}%1$c";
+	char *c="/*%1$c%2$cCommentaire1%1$c*/%1$c%1$cvoid%2$cfunction1()%1$c{%1$c%2$cchar *c=%3$c%s%3$c;%1$c%2$cprintf(c,10,9,34,c);%1$c}%1$c%1$cmain()%1$c{%1$c%2$c/*%1$c%2$c%2$cCommentaire2%1$c%2$c*/%1$c%2$cfunction1();%1$c}%1$c";
 	printf(c,10,9,34,c);
 }
 
 main()
 {
-	//Commentaire2
+	/*
+		Commentaire2
+	*/
 	function1();
 }
