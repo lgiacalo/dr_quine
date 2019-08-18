3ed70edf56a50a557e06fa7a34c3ee82c0fcc531
Lena GIACALONE <lgiacalo@student.42.fr>
2019-08-18 21:40:48 +0200

    ADD: Makefile

---
+++ C/Colleen.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/*
+	Commentaire1
+*/
+
+void	function1()
+{
+	char *c="#include<stdio.h>%1$c%1$c/*%1$c%2$cCommentaire1%1$c*/%1$c%1$cvoid%2$cfunction1()%1$c{%1$c%2$cchar *c=%3$c%s%3$c;%1$c%2$cprintf(c,10,9,34,c);%1$c}%1$c%1$cvoid%2$cmain()%1$c{%1$c%2$c/*%1$c%2$c%2$cCommentaire2%1$c%2$c*/%1$c%2$cfunction1();%1$c%2$creturn(0);%1$c}%1$c";
+	printf(c,10,9,34,c);
+}
+
+int	main()
+{
+	/*
+		Commentaire2
+	*/
+	function1();
+	return(0);
+}
