8b983584dc96c0b3eecafd1473f882a34b24520d
Lena GIACALONE <lgiacalo@student.42.fr>
2019-08-19 21:35:39 +0200

    clean

--- tests/example.c
+++ tests/example.c
@@ -1,5 +1,10 @@
+#include<stdio.h>
+#include<fcntl.h>
+
 main()
 {
 	char *c="main()%1$c{%1$c%2$cchar *c=%3$c%s%3$c;%1$c%2$cprintf(c,10,9,34,c);%1$c}";
 	printf(c,10,9,34,c);
+
+	printf("Valeur option open : O_CREAT = %d || O_WRONLY = %d\n", O_CREAT, O_WRONLY);
 }
