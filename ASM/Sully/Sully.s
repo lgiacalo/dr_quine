f54436e93d0d76cfa42726eedce7e27b140d9dae
Lena GIACALONE <lgiacalo@student.42.fr>
2019-08-20 14:57:15 +0200

    ASM: en cours sully.s

--- ASM/Sully/Sully.s
+++ ASM/Sully/Sully.s
@@ -1,9 +1,10 @@
 extern main, dprintf, system
 
 section .data
-	fmt:	db "#include<stdio.h>%1$c%1$cint main(void){printf(%3$cmerde [%5$d]\n%3$c);}", 0
-	filename: db "Sully_5.c", 0
-	cmpp: db "gcc -Wall -Wextra -Werror -o Sully_5 Sully_5.c", 0
+	fmt:	db "extern main, printf%1$csection .data%1$cfmt:	db %3$cMERDE%3$c, 0%1$csection .text%1$cfunction1:%1$cpush rbp%1$cmov rbp, rsp%1$cmov rdi, fmt%1$cmov rsi, 10%1$cmov rdx, 9%1$cmov rcx, 34%1$cmov r8, fmt%1$ccall printf%1$cleave%1$cret%1$c%1$cmain:%1$cpush rbp%1$cmov rbp, rsp%1$ccall function1%1$cleave%1$cret%1$c%1$c", 0
+	filename: db "Sully_5.s", 0
+	cmpo: db "nasm -f macho64 --prefix _ -Wall -Werror -o Sully_5.o Sully_5.s", 0
+	cmpp: db "cc -o Sully_5 Sully_5.o", 0
 	execc: db "./Sully_5", 0
 
 section .text
@@ -33,6 +34,9 @@ main:
 	mov r8, 34	;3
 	mov r9, fmt	;4
 	call dprintf
+
+	mov rdi, cmpo
+	call system
 
 	mov rdi, cmpp
 	call system
