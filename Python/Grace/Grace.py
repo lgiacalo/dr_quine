3c9b94ea25d1769bc5a7c9299d5082d74bd4671c
Lena GIACALONE <lgiacalo@student.42.fr>
2019-08-24 11:53:43 +0200

    Grace: Python ok

--- Python/Grace/Grace.py
+++ Python/Grace/Grace.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
 import sys
-"""
-	Colleen python program
-"""
+#
+#	Grace python program
+#
 
-str="#!/usr/bin/python{0}import sys{0}{2}{2}{2}{0}{1}Colleen python program{0}{2}{2}{2}{0}{0}str={2}{3}{2}{0}{0}fd=open('Grace_kid.py','w',0o664){0}sys.stdout=fd{0}print(str.format(chr(10),chr(9),chr(34),str)){0}sys.stdout.close(){0}fd.close()"
+str="#!/usr/bin/python{0}import sys{0}#{0}#{1}Grace python program{0}#{0}{0}str={2}{3}{2}{0}{0}try:{0}{1}fd=open('Grace_kid.py','w'){0}{1}sys.stdout=fd{0}{1}print(str.format(chr(10),chr(9),chr(34),str)){0}{1}sys.stdout.close(){0}{1}fd.close(){0}except:{0}{1}print({2}Error{2})"
 
-fd=open('Grace_kid.py','w')
-sys.stdout=fd
-print(str.format(chr(10),chr(9),chr(34),str))
-sys.stdout.close()
-fd.close()
+try:
+	fd=open('Grace_kid.py','w')
+	sys.stdout=fd
+	print(str.format(chr(10),chr(9),chr(34),str))
+	sys.stdout.close()
+	fd.close()
+except:
+	print("Error")
