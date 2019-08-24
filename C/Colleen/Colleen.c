f89f73d88726a2c014cfd4c73b6391f312f58ca4
Lena GIACALONE <lgiacalo@student.42.fr>
2019-08-24 11:15:32 +0200

    Colleen : C ok

--- C/Colleen/Colleen.c
+++ C/Colleen/Colleen.c
@@ -6,7 +6,7 @@
 
 void	function1()
 {
-	char *c="#include<stdio.h>%1$c%1$c/*%1$c%2$cCommentaire1%1$c*/%1$c%1$cvoid%2$cfunction1()%1$c{%1$c%2$cchar *c=%3$c%s%3$c;%1$c%2$cprintf(c,10,9,34,c);%1$c}%1$c%1$cvoid%2$cmain()%1$c{%1$c%2$c/*%1$c%2$c%2$cCommentaire2%1$c%2$c*/%1$c%2$cfunction1();%1$c%2$creturn(0);%1$c}%1$c";
+	char *c="#include<stdio.h>%1$c%1$c/*%1$c%2$cCommentaire1%1$c*/%1$c%1$cvoid%2$cfunction1()%1$c{%1$c%2$cchar *c=%3$c%s%3$c;%1$c%2$cprintf(c,10,9,34,c);%1$c}%1$c%1$cint%2$cmain()%1$c{%1$c%2$c/*%1$c%2$c%2$cCommentaire2%1$c%2$c*/%1$c%2$cfunction1();%1$c%2$creturn(0);%1$c}%1$c";
 	printf(c,10,9,34,c);
 }
 
