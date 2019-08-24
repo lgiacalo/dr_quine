78d0c779e1515cc617065cde62da4441fd16eb51
Lena GIACALONE <lgiacalo@student.42.fr>
2019-08-24 11:39:45 +0200

    Grace: ASM ok

--- ASM/Grace/Grace.s
+++ ASM/Grace/Grace.s
@@ -1,5 +1,5 @@
 ;programme Grace
-%define str "Grace_kid.c"
+%define str "Grace_kid.s"
 %define two 2
 %define three 3
 
@@ -7,7 +7,7 @@
 extern main, dprintf
 
 section .data
-	fmt:	db ";programme Grace%1$c%%define str %3$cGrace_kid.c%3$c%1$c%%define two 2%1$c%%define three 3%1$c%1$c%%macro mainn 0%1$cextern main, dprintf%1$c%1$csection .data%1$c%2$cfmt:%2$cdb %3$c%4$s%3$c, 0%1$c%2$cfilename: db %3$cGrace_kid.c%3$c, 0%1$c%1$csection .text%1$c%1$cmain:%1$c%2$cpush rbp%1$c%2$cmov rbp, rsp%1$c%1$c%2$cmov rax, 0x2000005%1$c%2$cmov rdi, filename%1$c%2$cmov rsi, 513%1$c%2$cmov rdx, 0644o%1$c%2$csyscall%1$c%1$c%2$cmov rdi, rax%1$c%2$cmov rsi, fmt%1$c%2$cmov rdx, 10%1$c%2$cmov rcx, 9%1$c%2$cmov r8, 34%1$c%2$cmov r9, fmt%1$c%2$ccall dprintf%1$c%1$c%2$cleave%1$c%2$cret%1$c%1$c%%endmacro%1$cmainn%1$c", 0
+	fmt:	db ";programme Grace%1$c%%define str %3$cGrace_kid.s%3$c%1$c%%define two 2%1$c%%define three 3%1$c%1$c%%macro mainn 0%1$cextern main, dprintf%1$c%1$csection .data%1$c%2$cfmt:%2$cdb %3$c%4$s%3$c, 0%1$c%2$cfilename: db %3$cGrace_kid.s%3$c, 0%1$c%1$csection .text%1$c%1$cmain:%1$c%2$cpush rbp%1$c%2$cmov rbp, rsp%1$c%1$c%2$cmov rax, 0x2000005%1$c%2$cmov rdi, filename%1$c%2$cmov rsi, 513%1$c%2$cmov rdx, 0644o%1$c%2$csyscall%1$c%1$c%2$cmov rdi, rax%1$c%2$cmov rsi, fmt%1$c%2$cmov rdx, 10%1$c%2$cmov rcx, 9%1$c%2$cmov r8, 34%1$c%2$cmov r9, fmt%1$c%2$ccall dprintf%1$c%1$c%2$cleave%1$c%2$cret%1$c%1$c%%endmacro%1$cmainn%1$c", 0
 	filename: db "Grace_kid.s", 0
 
 section .text
